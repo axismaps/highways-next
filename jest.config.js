47322c62f3249a7a2009dbfa9c275a29f0378179
David Heyman <dave@axismaps.com>
2021-04-02 08:02:32 +0100

    Wrote firsrt test

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
+  setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '\\.svg$': '<rootDir>/__mocks__/svgrMock.js',
+  },
+};
