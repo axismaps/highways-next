39d754f93a52fab49650a699dc6dab1157616a1d
David Heyman <dave@axismaps.com>
2021-04-02 15:50:14 +0100

    Use snapshot test for legend

--- components/Legend/Legend.test.js
+++ components/Legend/Legend.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import axios from 'axios';
+import renderer from 'react-test-renderer';
 import { render, screen } from '../../test/test-utils';
 
 import Legend from './index';
@@ -9,8 +10,16 @@ describe('Legend', () => {
     await axios.get(`${process.env.NEXT_PUBLIC_SEARCH_API}/layers?year=1950`);
     render(<Legend year={1950} />);
     await screen.findByText('Legend');
-
     expect(screen.getByRole('heading', { name: 'Legend' })).toBeInTheDocument();
     expect(screen.getByText('Roads')).toBeInTheDocument();
+  });
+
+  it('matches snapshot', async () => {
+    await axios.get(`${process.env.NEXT_PUBLIC_SEARCH_API}/layers?year=1950`);
+    render(<Legend year={1950} />);
+    await screen.findByText('Legend');
+
+    const tree = renderer.create(<Legend year={1950} />).toJSON();
+    expect(tree).toMatchSnapshot();
   });
 });
