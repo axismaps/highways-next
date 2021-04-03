f31aea4bdd79e46ea9fa97c5e509087aa6e1b7a7
David Heyman <dave@axismaps.com>
2021-04-03 09:53:08 +0100

    Use mock data for legend test

--- components/Legend/Legend.test.js
+++ components/Legend/Legend.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import renderer from 'react-test-renderer';
+import MockAdapter from 'axios-mock-adapter';
 import { render, screen } from '../../test/test-utils';
 
 import Legend from './index';
+import layers from '../../__mocks__/layers.data';
 
 describe('Legend', () => {
+  const mock = new MockAdapter(axios);
+  mock.onGet(`${process.env.NEXT_PUBLIC_SEARCH_API}/layers?year=1950`).reply(200, layers);
+
   it('renders for 1950 without crashing', async () => {
-    await axios.get(`${process.env.NEXT_PUBLIC_SEARCH_API}/layers?year=1950`);
     render(<Legend year={1950} />);
     await screen.findByText('Legend');
     expect(screen.getByRole('heading', { name: 'Legend' })).toBeInTheDocument();
@@ -15,7 +19,6 @@ describe('Legend', () => {
   });
 
   it('matches snapshot', async () => {
-    await axios.get(`${process.env.NEXT_PUBLIC_SEARCH_API}/layers?year=1950`);
     render(<Legend year={1950} />);
     await screen.findByText('Legend');
 
