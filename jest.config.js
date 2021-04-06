7f845786e87be236ef05253f2c229f12336dff55
David Heyman <dave@axismaps.com>
2021-04-06 10:27:09 +0100

    Use emotion serializer for snapshot tests

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,7 @@ module.exports = {
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFiles: ['dotenv-flow/config'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
+  snapshotSerializers: ['@emotion/jest/serializer'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
     '\\.svg$': '<rootDir>/__mocks__/svgrMock.js',
