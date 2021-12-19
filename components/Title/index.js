8bfca8a09a94cae669015f1935847765c7eb4aba
David Heyman <dave@axismaps.com>
2021-12-19 21:40:22 +0000

    URL update

--- components/Title/index.js
+++ components/Title/index.js
@@ -15,7 +15,7 @@ const Title = ({ title }) => {
         size="sm"
         borderRadius="full"
         as="a"
-        href="https://spatialstudieslab.rice.edu"
+        href="https://spatialstudieslab.rice.edu/houstonproject/highwayswaterways/"
         target="_blank"
         icon={<FontAwesomeIcon icon={faInfo} />}
       />
