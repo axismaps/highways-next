f4f438e5c68eabc4a1d0a49ea154a39ac4909d4d
David Heyman <dave@axismaps.com>
2022-03-25 08:39:34 +0000

    Added folders

--- components/Legend/index.js
+++ components/Legend/index.js
@@ -14,7 +14,7 @@ const fetcher = url => axios.get(url).then(({ data }) => data);
 
 const Legend = ({ year, layerHandler, highlightedLayer }) => {
   const debouncedYear = useDebounce(year, 500);
-  const { data: layers, error } = useSWR(
+  const { data: folders, error } = useSWR(
     debouncedYear ? `${process.env.NEXT_PUBLIC_SEARCH_API}/layers?year=${debouncedYear}` : null,
     fetcher
   );
@@ -22,7 +22,7 @@ const Legend = ({ year, layerHandler, highlightedLayer }) => {
   const isLayerActive = (layer, type) =>
     highlightedLayer && highlightedLayer.layer === layer.name && highlightedLayer.type === type;
 
-  if (!layers || error) return <Loading />;
+  if (!folders || error) return <Loading />;
 
   return (
     <Box>
@@ -30,38 +30,45 @@ const Legend = ({ year, layerHandler, highlightedLayer }) => {
         Legend
       </Heading>
       <Box px="15px" pb="10px" pt="1px" backgroundColor="white" borderRadius="5px" boxShadow="md">
-        {layers.map(layer => (
-          <Box key={layer.id}>
-            <Heading size="sm" mt={4} mb={2}>
-              {layer.title}
+        {folders.map(folder => (
+          <React.Fragment key={folder.name}>
+            <Heading size="md" mt={4} mb={-1} borderBottom="1px solid black">
+              {folder.name}
             </Heading>
-            {layer.types.map(type => {
-              const active = isLayerActive(layer, type);
-              return (
-                <Flex
-                  key={type}
-                  alignItems="center"
-                  p="5px"
-                  pr="10px"
-                  borderRadius="5px"
-                  lineHeight={1.25}
-                  backgroundColor={active ? '#eee' : 'none'}
-                  cursor="pointer"
-                  onClick={() => layerHandler(active ? null : { layer: layer.name, type })}
-                >
-                  <Box {...getColor(layer, type)} w="40px" h="20px" mr="10px" />
-                  <Text>{type}</Text>
-                  <Spacer />
-                  <Box ml="5px" color="#666">
-                    <FontAwesomeIcon
-                      icon={faWindowClose}
-                      visibility={active ? 'visible' : 'hidden'}
-                    />
-                  </Box>
-                </Flex>
-              );
-            })}
-          </Box>
+            {folder.layers.map(layer => (
+              <Box key={layer.id}>
+                <Heading size="sm" mt={4} mb={2}>
+                  {layer.title}
+                </Heading>
+                {layer.types.map(type => {
+                  const active = isLayerActive(layer, type);
+                  return (
+                    <Flex
+                      key={type}
+                      alignItems="center"
+                      p="5px"
+                      pr="10px"
+                      borderRadius="5px"
+                      lineHeight={1.25}
+                      backgroundColor={active ? '#eee' : 'none'}
+                      cursor="pointer"
+                      onClick={() => layerHandler(active ? null : { layer: layer.name, type })}
+                    >
+                      <Box {...getColor(layer, type)} w="40px" h="20px" mr="10px" />
+                      <Text>{type}</Text>
+                      <Spacer />
+                      <Box ml="5px" color="#666">
+                        <FontAwesomeIcon
+                          icon={faWindowClose}
+                          visibility={active ? 'visible' : 'hidden'}
+                        />
+                      </Box>
+                    </Flex>
+                  );
+                })}
+              </Box>
+            ))}
+          </React.Fragment>
         ))}
       </Box>
     </Box>
